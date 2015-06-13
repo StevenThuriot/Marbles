181c6e54e4c824b5d8c449d8e2711946485b8308
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-06-14 01:07:01 +0200

    author pagination

--- _plugins/author_generator.rb
+++ _plugins/author_generator.rb
@@ -7,12 +7,11 @@ module Jekyll
       @site = site
       @base = base
       @dir  = File.join((site.config['author_dir'] || 'authors'), slug)
-      @name = 'index.html'
-      @first = index == 0
-      @last = index == pageCount - 1
-                
-      if !first
-          @name = site.config['paginate_path'].gsub! ':num', (index+1).to_s
+      @name = 'index.html'      
+        
+      if index != 0          
+          path = site.config['paginate_path']
+          @name =  path.sub(':num', (index+1).to_s)
       end
      
       self.process(@name)
@@ -21,12 +20,6 @@ module Jekyll
       self.data['author'] = author
       self.data['dir'] = dir
       self.data['title'] = "#{author['name']}"
-      self.data['posts'] = posts
-      self.data['first'] = first
-      self.data['last'] = last
-      self.data['pageCount'] = pageCount
-      
-      #todo : add fake paginator so we can reuse the pagination page from the real paginator
     end
   end
 
@@ -52,11 +45,31 @@ module Jekyll
 
 
   class Site
-
-    def write_author_index(slug, author, posts, index, pageCount)  
+      
+    def write_author_index(slug, author, posts, index, pageCount, postCount)  
+      paginator = { "page" => index + 1,
+                    "per_page" => self.config['paginate'],
+                    "posts" => posts,
+                    "total_posts" => postCount,
+                    "total_pages" => pageCount }
+      
+      if  index != 0
+          paginator['previous_page'] = index
+          path = self.config['paginate_path']
+          paginator['previous_page_path'] = path.sub(':num', (index).to_s)
+      end
+      
+      if index != (pageCount - 1)
+          paginator['next_page'] = index+2
+          path = self.config['paginate_path']
+          paginator['next_page_path'] = path.sub(':num', (index+2).to_s)
+      end 
+    
+      payload =  site_payload
+      payload['paginator'] = paginator
         
       index = AuthorIndex.new(self, self.source, slug, author, posts, index, pageCount)
-      index.render(self.layouts, site_payload)
+      index.render(self.layouts, payload)
       index.write(self.dest)
         
       # Record that this page has been added, otherwise Site::cleanup will remove it.
@@ -92,9 +105,13 @@ module Jekyll
               
             self.write_author_feed(slug, author, posts)  
             
-            slicedPosts = posts.each_slice(5)              
-            slicedPosts.each_with_index do |slicedPosts, i|
-                self.write_author_index(slug, author, slicedPosts, i, slicedPosts.size) 
+            if self.config['paginate']
+                slicedPosts = posts.each_slice(5).to_a             
+                slicedPosts.each_with_index do |slice, i|
+                    self.write_author_index(slug, author, slice, i, slicedPosts.size, posts.size) 
+                end
+            else
+                self.write_author_index(slug, author, posts, 0, posts.size, posts.size) 
             end
           end
       else
